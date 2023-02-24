8fe2be375bbcde7e87ceefbb85467abfe7001a07
Dan M <dan@marrera.com>
2023-02-24 13:09:21 -0600

    adding replace_values stub configuration

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     py_modules=[
         'print_outputs',
         'empty_bucket',
+        'replace_values',
     ],
     entry_points={
         'sceptre.hooks': [
-            'print_outputs = hooks.print_outputs:PrintStackOutputs',
-            'empty_bucket  = hooks.empty_bucket:EmptyBucket',
+            'print_outputs   = hooks.print_outputs:PrintStackOutputs',
+            'empty_bucket    = hooks.empty_bucket:EmptyBucket',
+            'replace_values  = hooks.replace_values:ReplaceValues',
         ]
     }
 )
