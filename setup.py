1dd556ceaddf361918a25615b1524dd91e3d23eb
Dan M <dan@marrera.com>
2023-02-24 19:27:17 -0600

    removing requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
 setup(
-    name="sceptre_hooks",
-    packages=['hooks'],
-    # py_modules=[
-    #     'print_outputs',
-    #     'empty_bucket',
-    #     'replace_values',
-    # ],
+    name='sceptre_hooks',
+    version='0.0.1',
+    description='Sceptre Hooks',
+    author='Dan Marrera',
+    author_email='dan@marrera.com',
+    py_modules=[
+        'hooks.print_outputs',
+        'hooks.empty_bucket',
+        'hook.sreplace_values',
+    ],
+    requires=[
+        'boto3',
+        'sceptre',
+        'setuptools',
+    ],
     entry_points={
         'sceptre.hooks': [
             'print_outputs = hooks.print_outputs:PrintStackOutputs',
